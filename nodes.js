2001beca1fd33853f71c303cf060019c98c8922f
vishal2599 <vshl2599@gmail.com>
2023-05-08 17:10:30 +0530

    Search Fix

--- nodes.js
+++ nodes.js
@@ -1,56 +1,58 @@
-function Node(val, x, y){
+function Node(val, x, y) {
     this.value = val;
     this.left = null;
     this.right = null;
     this.x = x;
     this.y = y;
-}
-
-Node.prototype.search = function(val){
-    if(this.value == val){
-        return this;
-    } else if( this.left != null && val < this.value ){
-        return this.left.search();
-    } else if( this.right != null && val > this.value ){
-        return this.right.search();
+  }
+  
+  Node.prototype.search = function(val) {
+    if (this.value == val) {
+      return this;
+    } else if (val < this.value && this.left != null) {
+      return this.left.search(val);
+    } else if (val > this.value && this.right != null) {
+      return this.right.search(val);
     }
     return null;
-}
-
-Node.prototype.visit = function(parent){
-    if( this.left != null ){
-        this.left.visit(this);
+  }
+  
+  Node.prototype.visit = function(parent) {
+    if (this.left != null) {
+      this.left.visit(this);
     }
     console.log(this.value);
-    noStroke();
     fill(255);
+    noStroke();
     textAlign(CENTER);
     text(this.value, this.x, this.y);
-    noFill();
     stroke(255);
-    line(parent.x, parent.y, this.x, this.y);
+    noFill();
     ellipse(this.x, this.y, 20, 20);
-    if( this.right != null ){
-        this.right.visit(this);
+    line(parent.x, parent.y, this.x, this.y);
+    if (this.right != null) {
+      this.right.visit(this);
     }
-}
-
-Node.prototype.addNode = function(n){
-    if(n.value < this.value){
-        if(this.left == null){
-            this.left = n;
-            this.left.x = this.x - 50;
-            this.left.y = this.y + 20;
-        } else {
-            this.left.addNode(n);
-        }
-    } else if ( n.value > this.value ) {
-        if(this.right == null){
-            this.right = n;
-            this.right.x = this.x + 50;
-            this.right.y = this.y + 20;
-        } else {
-            this.right.addNode(n);
-        }
+  }
+  
+  Node.prototype.addNode = function(n) {
+    if (n.value < this.value) {
+      if (this.left == null) {
+        this.left = n;
+        this.left.x = this.x - 50;
+        this.left.y = this.y + 20;
+      } else {
+        this.left.addNode(n)
+      }
+    } else if (n.value > this.value) {
+      if (this.right == null) {
+        this.right = n;
+        this.right.x = this.x + 50;
+        this.right.y = this.y + 20;
+  
+      } else {
+        this.right.addNode(n);
+      }
     }
-}
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,20 +1,23 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
 var tree;
 
-function setup(){
-    createCanvas(600, 400);
-    background(61);
-    var n = new Node(5);
-    tree = new Tree();
-    for(var i = 0; i < 10; i++){
-        tree.addValue(Math.floor((Math.random() * 100) + 1) + i);
-    }
+function setup() {
+  createCanvas(600, 400);
+  background(51);
+  tree = new Tree();
+  for (var i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
+  console.log(tree);
+  tree.traverse();
 
-    tree.traverse();
-
-    var result = tree.search(10);
-    if( result == null ){
-        console.log('Not Found');
-    } else {
-        console.log(result);
-    }
+  var result = tree.search(10);
+  if (result == null) {
+    console.log('not found');
+  } else {
+    console.log(result);
+  }
 }
